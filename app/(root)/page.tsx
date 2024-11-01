3a37f9dbb5e34a47fb18eceffc745088280a7cb1
Arthur Laroya <arthur.j.laroya@gmail.com>
2024-11-01 17:15:46 +1100

    theming, hero, search form, react 19 server actions, init startup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query || "";
+
+  const posts = [];
+  // 1:57:45
+
   return (
     <>
-    <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> connect with Entrepeneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, vote on pitches, and get noticed in virtual
+          competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+
+        </ul>
+      </section>
     </>
   );
 }
